f41e084762b062d6a032cf36dbe2f52fee440a68
Sarah <emmanuelle-sarah.oyono.bisso@efrei.net>
2020-12-16 12:03:20 +0100

    Upadate jenkins

--- jenkins.Develop.groovy
+++ jenkins.Develop.groovy
@@ -3,11 +3,12 @@ def build_app(){
 }
 
 def test_app(){
-  sh 'python test_app.py'
+  sh 'python -m unittest test_app.py'
 }
 
 def stress_test_app() {
-    echo 'http:127.0.0.1:5000/?{1..1000} | xags -n 1 -P 1000 curl -s'
+    //echo 'http:127.0.0.1:5000/?{1..1000} | xags -n 1 -P 1000 curl -s'
+    echo 'Stress test'
 }
 
 def down_app(){
